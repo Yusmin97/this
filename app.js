d7375047bb8d497086b9e0cd3c73df9aa0563f58
Yusmin97 <tmdamsi97@gmail.com>
2023-10-11 10:39:46 +0900

    this 연습

---
+++ app.js
@@ -0,0 +1,11 @@
+function a(callback) {
+  return callback();
+}
+
+console.log(
+  a(function () {
+    console.log(this);
+    console.log('--------------------------');
+    console.log(global);
+  })
+);
